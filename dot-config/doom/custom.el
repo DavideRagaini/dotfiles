88ac63fb941d05953466eb6386f7005b36ee1faf
DavideRagaini <DavideRagaini@gmail.com>
2021-05-26 20:00:45 +0200

    Whitespaces style

--- dot-config/doom/custom.el
+++ dot-config/doom/custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(flycheck-checker-error-threshold 500))
+ '(flycheck-checker-error-threshold 500)
+ '(whitespace-style
+   '(face trailing tabs spaces newline empty indentation::tab indentation::space indentation big-indent space-after-tab::tab space-after-tab::space space-after-tab space-before-tab::tab space-before-tab::space space-before-tab space-mark tab-mark newline-mark)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
