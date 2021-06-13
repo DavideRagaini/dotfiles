d115582c5abbf2fce72de1e3239170ce6ef5dac1
DavideRagaini <DavideRagaini@gmail.com>
2021-06-13 09:11:17 +0200

    minimal

--- dot-config/qutebrowser/config.py
+++ dot-config/qutebrowser/config.py
@@ -1,16 +1,12 @@
-config.load_autoconfig()
+import dracula
 
-config.bind(",a", ':hint links spawn mpv "{hint-url}"')
-config.bind(",da", ':hint links spawn dy audio "{hint-url}"')
-config.bind(",dba", ':hint links spawn dy bestaudio "{hint-url}"')
-config.bind(",dbv", ':hint links spawn dy bestvideo "{hint-url}"')
-config.bind(",dv", ':hint links spawn dy video "{hint-url}"')
+config.load_autoconfig()
+dracula.blood(c, {"spacing": {"vertical": 6, "horizontal": 8}})
+
+config.bind(",a", ':hint links spawn dmpv aplay "{hint-url}"')
 config.bind(",e", ':hint links spawn dmpv enqueue "{hint-url}"')
-config.bind(",i", ':hint links spawn getpic "{hint-url}"')
 config.bind(",p", ':hint links spawn dmpv enqueue&play "{hint-url}"')
-config.bind(",q", ':hint links spawn dmpv queue "{hint-url}"')
 config.bind(",t", ':hint links spawn transadd "{hint-url}"')
-config.bind(",w", ':hint links spawn winmpv play "{hint-url}"')
 
 c.url.searchengines = {
     "DEFAULT": "https://searx.nevrlands.de/search?q={}",
@@ -18,9 +14,5 @@ c.url.searchengines = {
     "g": "https://www.google.com/search?q={}",
     "w": "https://en.wikipedia.org/?search={}",
     "y": "https://www.youtube.com/results?search_query={}",
-    "aw": "https://wiki.archlinux.org/?search={}",
+    "a": "https://wiki.archlinux.org/?search={}",
 }
-
-import dracula
-
-dracula.blood(c, {"spacing": {"vertical": 6, "horizontal": 8}})
