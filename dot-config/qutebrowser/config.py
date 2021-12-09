77c57ec79cb6cfb7b36ec32d2b93c52221dd5bff
DavideRagaini <DavideRagaini@gmail.com>
2021-12-09 10:59:38 +0100

    qtb darkmode and editor/visual env var

--- dot-config/qutebrowser/config.py
+++ dot-config/qutebrowser/config.py
@@ -100,3 +100,13 @@ c.fileselect.folder.command = fileselect_cmd
 c.fileselect.multiple_files.command = fileselect_cmd
 c.fileselect.single_file.command = fileselect_cmd
 # }}}
+
+# ======================= Dark Mode ============= {{{
+# c.colors.webpage.prefers_color_scheme_dark = True
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.algorithm = 'lightness-hsl'
+c.colors.webpage.darkmode.contrast = -.022
+c.colors.webpage.darkmode.threshold.text = 150
+c.colors.webpage.darkmode.threshold.background = 100
+c.colors.webpage.darkmode.policy.images = 'always'
+c.colors.webpage.darkmode.grayscale.images = 0.35
