2399c022aa6ec2c90f32ff7e82c05d59dde644c0
DavideRagaini <DavideRagaini@gmail.com>
2022-01-21 11:36:45 +0100

    Fix

--- dot-config/qutebrowser/greasemonkey/libreddit-redirect.js
+++ dot-config/qutebrowser/greasemonkey/libreddit-redirect.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name           Libreddit redirector
 // @namespace      raghyz's userscripts
-// @match          http://www.youtube.com/*
-// @match          https://www.youtube.com/*
-// @match          http://youtube.com/*
-// @match          https://youtube.com/*
+// @match          http://www.reddit.com/*
+// @match          https://www.reddit.com/*
+// @match          http://reddit.com/*
+// @match          https://reddit.com/*
 // @run-at         document-start
 // ==/UserScript==
 location.host = "r.nf";
