bba5ea418b8995ef975fb828444fe8f5f4ce2965
Davide Ragaini <ragainidavide@gmail.com>
2022-07-26 17:49:56 +0200

    Synced

--- dot-config/qutebrowser/greasemonkey/libreddit-redirect.js
+++ dot-config/qutebrowser/greasemonkey/libreddit-redirect.js
@@ -5,6 +5,10 @@
 // @match          https://www.reddit.com/*
 // @match          http://reddit.com/*
 // @match          https://reddit.com/*
+// @match          http://www.old.reddit.com/*
+// @match          https://www.old.reddit.com/*
+// @match          http://old.reddit.com/*
+// @match          https://old.reddit.com/*
 // @run-at         document-start
 // ==/UserScript==
 location.host = "r.nf";
